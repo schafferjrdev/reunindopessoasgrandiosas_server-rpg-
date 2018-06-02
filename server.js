5b1aefd61ac88a69c12f084068df88e6c6fd6ddf
schafferjrdev <mr.giancarloschaffer@gmail.com>
2018-06-02 12:51:05 -0300

    Upload procfile

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const appRootPath 	= require('app-root-path');
 const app 			= appRootPath.require('./app');  
 const mysql 		= require('mysql');
  
-const port = process.env.PORT || 3070;   
+const port = process.env.PORT || 8080;   
 
 app.listen(port, function () {   
 	console.log('Servidor na porta:', port)
