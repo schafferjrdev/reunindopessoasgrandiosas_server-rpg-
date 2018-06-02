1f45424b2373956bdc1083ca7b53f27258eb4aa0
schafferjrdev <mr.giancarloschaffer@gmail.com>
2018-06-02 13:02:16 -0300

    Reup Procfile prot 8080

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const appRootPath 	= require('app-root-path');
 const app 			= appRootPath.require('./app');  
 const mysql 		= require('mysql');
  
-const port = process.env.PORT || 3070;   
+const port = process.env.PORT || 8080;   
 
 app.listen(port, function () {   
-	console.log('Servidor na porta:', port)
+	console.log('Server listening on: http://localhost:%s', port)
 	 })
