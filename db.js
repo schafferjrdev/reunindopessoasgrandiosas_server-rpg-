005f36d447fae5f2346d01815945abdf3f7c0738
schafferjrdev <mr.giancarloschaffer@gmail.com>
2018-06-02 14:10:21 -0300

    db update

--- db.js
+++ db.js
@@ -2,12 +2,13 @@ const mysql = require('mysql');
 
 let con = () => {
     return mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        port: 3308,
-        database: 'bancobixossmd'
+        host: 'sql10.freemysqlhosting.net',
+        user: 'sql10241069',
+        password: 'Xv2lBwXhDq',
+        port: 3306,
+        database: 'sql10241069'
     });
 };
+
 
 module.exports = con;
