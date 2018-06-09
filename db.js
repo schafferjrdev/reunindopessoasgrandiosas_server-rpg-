b2ff8c7510915895beed61569ac6038a99901c70
schafferjrdev <mr.giancarloschaffer@gmail.com>
2018-06-09 20:41:07 -0300

    novo banco

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql');
 let con = () => {
     return mysql.createConnection({
         host: 'sql10.freemysqlhosting.net',
-        user: 'sql10241069',
-        password: 'Xv2lBwXhDq',
+        user: 'sql10242187',
+        password: 'Ih3psvg77J',
         port: 3306,
-        database: 'sql10241069'
+        database: 'sql10242187'
     });
 };
 
