55e37b554fcc6df688df35f4ff16d4f5b1c90779
schafferjrdev <30741185+schafferjrdev@users.noreply.github.com>
2018-06-15 23:12:52 -0300

    change to old

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 
 let con = () => {
     return mysql.createConnection({
-        host: 'http://mysql.hostinger.com.br/',
-        user: 'bdbixosm_ducksmd',
-        password: 'P4T0SSMDmestre',
+        host: 'sql10.freemysqlhosting.net',
+        user: 'sql10242187',
+        password: 'Ih3psvg77J',
         port: 3306,
-        database: 'bdbixosm_bixosmd'
+        database: 'sql10242187'
     });
 };
 
